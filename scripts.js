60c8406a997d39f9a66beae5d5cf5fafcefe7fcf
Giovanni Bernini <gbernini@redventures.com>
2020-03-10 10:36:45 -0300

    fix: removing log

--- scripts.js
+++ scripts.js
@@ -4,8 +4,6 @@ const params = new URLSearchParams(url)
 const target = params.get('target')
 const fallback = params.get('fallback')
 
-console.log({ target, fallback })
-
 if (target) {
 	setTimeout(() => {
 		if(fallback) window.location.href = fallback
